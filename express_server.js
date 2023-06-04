d38404d60b2e6d9fab677159f9b7f87b0a2f98e0
mattyu123 <matthewyu100@gmail.com>
2023-06-04 19:42:44 -0400

    install ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,6 +21,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//listens for a connection to the server and returns to the user that the connection has been established 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
