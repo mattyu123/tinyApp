66383b66b5abb1aaf2c62933939065cd4505b482
mattyu123 <matthewyu100@gmail.com>
2023-06-05 16:46:37 -0400

    add generateRandomString function to generate a random 6 character alphanumerical string

--- express_server.js
+++ express_server.js
@@ -10,6 +10,18 @@ app.use(express.urlencoded({ extended: true }));
 //middleware to use ejs
 app.set("view engine", "ejs");
 
+//function to generate a random unique 6 character string
+function generateRandomString() {
+  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+
+  for ( let i = 0; i < 6; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -28,6 +40,12 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//sends a post request to the server
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(`Status: ${res.statusCode} Ok`); 
+});
+
 //new route to render template with access to specific url id
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -35,16 +53,17 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello! You have reached the home page");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+//old code at the beginning, commenting out for now 
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 //listens for a connection to the server and returns to the user that the connection has been established
 app.listen(PORT, () => {
