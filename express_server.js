b2e5fd433d3b5f584dfdd2a2fc801c0be93ef56d
mattyu123 <matthewyu100@gmail.com>
2023-06-05 18:05:15 -0400

    run linter through code

--- express_server.js
+++ express_server.js
@@ -11,13 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 //function to generate a random unique 6 character string
-function generateRandomString() {
-  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const generateRandomString = function() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
   const charactersLength = characters.length;
 
-  for ( let i = 0; i < 6; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  for ( let i = 0; i < 6; i++ ){
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
@@ -42,20 +42,19 @@ app.get("/urls/new", (req, res) => {
 
 //sends a post request to the server
 app.post("/urls", (req, res) => {
-  // console.log(req.body); // Log the POST request body to the console
   
-  const id = generateRandomString()
-  const longURL = req.body.longURL
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
 
-  urlDatabase[id] = longURL //updates the urlDatabase with the random shortened URL and the submitted form URL
+  urlDatabase[id] = longURL; //updates the urlDatabase with the random shortened URL and the submitted form URL
 
-  res.redirect(`/urls/${id}`) //redirect to the new URL with the id in the path
+  res.redirect(`/urls/${id}`); //redirect to the new URL with the id in the path
 });
 
 //new route to render template with access to specific url id
 app.get("/urls/:id", (req, res) => {
-  const id = req.params.id
-  const longURL = urlDatabase[req.params.id]
+  const id = req.params.id;
+  const longURL = urlDatabase[req.params.id];
 
   const templateVars = { id, longURL };
   res.render("urls_show", templateVars);
@@ -63,13 +62,11 @@ app.get("/urls/:id", (req, res) => {
 
 //takes the short form URL and redirects it to the long form URL
 app.get("/u/:id", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
-  const longURL = urlDatabase[id]
+  const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello! You have reached the home page");
@@ -79,7 +76,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//old code at the beginning, commenting out for now 
+//old code at the beginning, commenting out for now
 // app.get("/hello", (req, res) => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
