95e20255ef9e8a8cf703775ea2ecd444c05c6586
mattyu123 <matthewyu100@gmail.com>
2023-06-09 16:57:05 -0400

    encrypt cookies with session

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session')
+// const cookieParser = require('cookie-parser');
 const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -8,7 +9,12 @@ const PORT = 8080; // default port 8080
 //setting up all the middleware that we need
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+// app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['pizza','potato','chips','phone'],
+  maxAge: 24 * 60 * 60 * 1000
+}))
 app.set("view engine", "ejs");
 
 //HELPER FUNCTIONS DEFINED BELOW
@@ -82,14 +88,14 @@ const users = {}
 //Request also takes the cookies that were generated
 app.get('/urls', (req, res) => {
   const id = req.params.id;
-  const loggedInUser = users[req.cookies.user_id];
+  const loggedInUser = users[req.session.user_id];
 
   if (loggedInUser === undefined) {
     res.render("loginErrorScreen")
     return;
   }
 
-  const finalURLs = urlsForUser(req.cookies.user_id, urlDatabase)
+  const finalURLs = urlsForUser(req.session.user_id, urlDatabase)
 
   // pass in only the urls that belong to the user
    const templateVars = {
@@ -102,7 +108,7 @@ app.get('/urls', (req, res) => {
 
 //create a route for the registration page when the user wants to login
 app.get('/register', (req, res) => {
-  const loggedInUser = users[req.cookies.user_id];
+  const loggedInUser = users[req.session.user_id];
   
   //if user is already logged in, redirects user to the /url page
   if (loggedInUser) {
@@ -145,13 +151,14 @@ app.post('/register', (req, res) => {
   };
   
   //create a cookie with the user's newly generated ID
-  res.cookie("user_id", newUserID);
+  // res.cookie("user_id", newUserID);
+  req.session.user_id = newUserID;
   res.redirect("/urls");
 });
 
 //Create a get route to render the urls_new.ejs
 app.get("/urls/new", (req, res) => {
-  const loggedInUser = users[req.cookies.user_id];
+  const loggedInUser = users[req.session.user_id];
   
   // if user is not logged in, they cannot add a new URL and redirects them to login page
   if (loggedInUser === undefined) {
@@ -160,7 +167,7 @@ app.get("/urls/new", (req, res) => {
   }
 
   const templateVars = {
-    user: users[req.cookies.user_id] 
+    user: users[req.session.user_id] 
   };
 
   res.render("urls_new", templateVars);
@@ -168,7 +175,7 @@ app.get("/urls/new", (req, res) => {
 
 //sends a post request to the server
 app.post("/urls", (req, res) => {
-  const loggedInUser = users[req.cookies.user_id];
+  const loggedInUser = users[req.session.user_id];
   
   // if user is not logged in, tell the user why they cannot shorten URLs
   if (loggedInUser === undefined) {
@@ -182,7 +189,7 @@ app.post("/urls", (req, res) => {
   //updated urlDatabase that will add the longURL, and the userID of the person adding the URL
   urlDatabase[id] = {
     longURL,
-    userID: req.cookies.user_id
+    userID: req.session.user_id
   }
 
   res.redirect(`/urls/${id}`); //redirect to the new URL with the id in the path
@@ -191,7 +198,7 @@ app.post("/urls", (req, res) => {
 //new route to render template with access to specific url id
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const loggedInUser = users[req.cookies.user_id];
+  const loggedInUser = users[req.session.user_id];
 
   console.log(loggedInUser)
 
@@ -214,7 +221,7 @@ app.get("/urls/:id", (req, res) => {
   }
 
   const longURL = urlDatabase[req.params.id]["longURL"];
-  const templateVars = { id, longURL, user: users[req.cookies.user_id], }; //this used to be username, changed it to user
+  const templateVars = { id, longURL, user: users[req.session.user_id], }; //this used to be username, changed it to user
   
   res.render("urls_show", templateVars);
 });
@@ -235,7 +242,7 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.post("/urls/:id", (res, req) => {
-  const loggedInUser = users[req.cookies.user_id];
+  const loggedInUser = users[req.session.user_id];
   const id = req.params.id;
 
   //check to see if the user is logged in or not
@@ -261,7 +268,7 @@ app.post("/urls/:id", (res, req) => {
 //post route that removes a url resources and redirects user back to url home page
 app.post("/urls/delete/:id", (req, res) => {
   const id = req.params.id;
-  const loggedInUser = users[req.cookies.user_id];
+  const loggedInUser = users[req.session.user_id];
 
   if (id === undefined) {
     res.send("This page does not exist");
@@ -293,7 +300,7 @@ app.post("/urls/edit/:id", (req, res) => {
 
 //route that renders the login page
 app.get("/login", (req, res) => {
-  const loggedInUser = users[req.cookies.user_id];
+  const loggedInUser = users[req.session.user_id];
   
   //if user is already logged in, redirects user to the /url page
   if (loggedInUser) {
@@ -320,13 +327,15 @@ app.post("/login", (req, res) => {
     return res.status(403).send("Your password does not match");
   }
 
-  res.cookie("user_id", checkLoginCredentials.id);
+  // res.cookie("user_id", checkLoginCredentials.id);
+  req.session.user_id = checkLoginCredentials.id;
   res.redirect("/urls");
 });
 
 //post route that will clear the user_id cookie
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  // res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/login");
 });
 
